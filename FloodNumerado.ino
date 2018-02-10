b370e762de7b275733e3191dff3601ec61baf92b
kammradt <34798570+kammradt@users.noreply.github.com>
2018-02-10 16:55:47 -0200

    Payload: FloodNumerado

---
+++ FloodNumerado.ino
@@ -0,0 +1,46 @@
+#include "Keyboard.h";
+//Declarar a variável que vai contar e imprimir
+int numero = 0;
+
+void typeKey(int key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+void setup()
+{
+// Programa baseado em 'loop'
+}
+
+void loop() 
+{
+  #include "Keyboard.h"
+  Keyboard.begin();
+  delay(250);
+  //Iniciar modo e delay do loop
+  
+  Keyboard.print("Mensagem - ");
+  Keyboard.println(numero);
+  //Digitar mensagem - Arrumar a separação de texto+variável
+  
+  Keyboard.press(KEY_RETURN);
+  //Enviar - Enter 
+  
+  Keyboard.releaseAll();
+  //Soltar
+  
+  numero++;
+  //Contador da variável 'numero'
+  
+  Keyboard.end();
+  //Finaliza o modo
+}
+
+/* Essa área foi destinada a testar o programa, já que toda vez que ele
+ é conectado ao pc, inicia o código em loop
+ 
+*/
+
+
